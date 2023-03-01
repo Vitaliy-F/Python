8b0af71453c14a5e17915281bd3285e7857e4aa7
John Doe <vitaliyfedor@gmail.com>
2023-03-01 17:18:55 +0200

    main

--- homework#2/main.py
+++ homework#2/main.py
@@ -1,14 +1,22 @@
-from utils.helpers import get_all_humans, write_new_human
+from utils.helpers import get_all_humans, write_new_human, check_email, change_items, is_email_correct
 
 while True:
-    print("1. Add new person \n2. Get all persons")
+    print("1. Add new person \n2. Get all persons \n3. Edit person")
     flag = int(input("Choose what youwant to do: "))
     if flag ==1:
         first_name = input("First Name: ")
         last_name = input("Last Name: ")
         email = input("Email: ")
-        data=get_all_humans()
-        human = {"first_name":first_name, "last_name":last_name,"email":email}
+        
+        while check_email(email) == False:  
+            print("Invalid Email") 
+            break
+
+        while is_email_correct(email) == False:
+            print("This Email already exist")
+            email = input("Enter new Email: ")
+        else:
+            human = {'first_name': first_name, "last_name": last_name, "email": email}
         write_new_human(human)
 
     elif flag ==2:
@@ -18,3 +26,10 @@ while True:
             print(human ["last_name"])
             print(human ["email"])
             print("-----------------------------------------------")
+
+    elif flag == 3:
+        change_id = int(input("Enter id to change item:"))
+        change_items(change_id)
+    else:
+        print("Don't have this item")
+        break
--- homework#2/utils/helpers.py
+++ homework#2/utils/helpers.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 def get_all_humans():
     f = open("homework#2\database\persons.json", "r")
@@ -17,3 +18,36 @@ def write_new_human(human):
     data = json.dumps(data)
     f.write(data)
     f.close()
+
+
+def check_email(email):
+    pat = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    if re.match(pat, email):
+        return True
+    else:
+        return False
+
+
+def change_items(input_id):
+    data = get_all_humans()
+    for item in data:
+        if item["id"] == int(input_id):
+            item["first_name"] = input("Edit your first name: ")
+            item["last_name"] = input("Edit your last name: ")
+            email = input("Edit your email: ")
+    file = open("homework#2\database\persons.json", "w")
+    data = json.dumps(data)
+    file.write(data)
+    file.close()
+
+
+def is_email_correct(e):
+    data = get_all_humans()
+    check_list = []
+    for item in data:
+        check_list.append(item["email"])
+    result = check_email(e)
+    if e in check_list:
+        return result and False
+    else:
+        return result and True
